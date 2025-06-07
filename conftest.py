f14e0f98ab3d776eb5121128701063426eea790f
micta <artemen23093@gmail.com>
2025-06-07 09:35:44 +0300

    fix: add headless and CI config

--- conftest.py
+++ conftest.py
@@ -2,13 +2,18 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from pages.login_page import LoginPage
 
 @pytest.fixture
 def browser():
-    # Налаштування драйвера
+    options = Options()
+    options.add_argument("--headless")  # важливо для CI
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
